411216c3f6371813340c9d74bdc6dcc12a985649
buffpojken <daniel@sykewarrior.com>
2013-10-25 16:55:02 +0200

    minor tweaks

--- app/models/property.rb
+++ app/models/property.rb
@@ -77,18 +77,6 @@ class Property < ActiveRecord::Base
 		return Property.new(:parent_id => self.id, :name => self.name, :property_klazz => self.property_klazz, :property_type_definition => self.property_type_definition, :default_value => self.value)
 	end
 
-	# def reload
-	# 	value_object.reload
-	# end
-
-	# def method_missing(name, *args, &block)
-	# 	if value_object.respond_to?(name)
-	# 		value_object.send(name, *args, &block)
-	# 	else
-	# 		super
-	# 	end
-	# end
-
 	private
 
 	def value_object
--- app/models/property_value.rb
+++
@@ -1,9 +0,0 @@
-class PropertyValue
-
-	embedded_in :game_object
-
-	field :name, type: String
-	field :default_value_id, type: String
-	field :value_id, type: String
-	
-end
