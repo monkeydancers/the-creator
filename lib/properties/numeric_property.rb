e6e74994d236aa03bb1f1ad865afed2173f34979
buffpojken <daniel@sykewarrior.com>
2013-11-04 11:17:04 +0100

    added tests for numeric property

--- lib/properties/numeric_property.rb
+++ lib/properties/numeric_property.rb
@@ -3,9 +3,17 @@ class NumericProperty < PropertyProxy
 		unless object_id
 			super
 		else
-			@value = $redis.get(object_id)
+			data = $redis.hgetall(object_id)
 			new({:id => object_id, :value => data['value'].to_i, :default_value => data['default_value'].to_i})
 		end
+	end
+
+	def value
+		@value.to_i
+	end
+
+	def default_value
+		@default_value.to_i
 	end
 
 	def stem
@@ -24,4 +32,13 @@ class NumericProperty < PropertyProxy
 	def self.can_set_property_klazz?
 		true
 	end
+
+	private
+
+	def refetch
+		data 						= $redis.hgetall(self.id)
+		@value 					= data['value']
+		@default_value 	= data['default_value']
+	end
+
 end
