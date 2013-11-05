3460a44b3e37565507c881891ddaacd7f3371c5b
buffpojken <daniel@sykewarrior.com>
2013-11-05 14:09:22 +0100

    began work on testsuite for rule engine

---
+++ test/models/engine_context_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class EngineContextTest < ActiveSupport::TestCase
+
+	context 'When setting up rule environment, the system' do 
+		should 'fail' do 
+			flunk("monkey")
+		end
+	end
+
+end
