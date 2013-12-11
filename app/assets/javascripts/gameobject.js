5c92df380b582a511069c64bc549d3f09da8581b
Staffan Rosenberg <staffan@monkeydancers.com>
2013-12-11 22:13:51 +0700

    Inline editing and tabbar javascript

--- app/assets/javascripts/gameobject.js
+++ app/assets/javascripts/gameobject.js
@@ -1,7 +1,138 @@
-window.gameobject = Object.create({
+window.editable = Object.create({
+	_edit_clicked: function(index, event){
+		var _t = this;
+		var edit_entry = $(event);
 
+		console.log(edit_entry.html());
+		console.log(edit_entry.data('key'));
+
+		_t.workspace.prepend(_t.template.render({
+			'header' 		: 'Editing ' + edit_entry.data('key'),
+			'value' 		: edit_entry.text(),
+			'identifier' 	: edit_entry.data('identifier') 
+		}));
+
+	},
+	init: function(workspace){
+		var _t 				= this;
+		_t.workspace 	= workspace;
+    	_t.template		= Liquid.parse($('#workspace_popin_template').html());
+
+		// Attach events
+    	_t.workspace.find('.editable').each(function( index){ 
+    		$(this).on('click.creator.editable', function() { _t._edit_clicked.apply(_t, [index, this]) } );
+    	});
+		return _t;
+	}
+});
+
+
+window.game_objects_tabs = Object.create({
+	_tab_clicked: function(index){
+		
+		// Switch content
+		this.tabs_content.find('.tab-content').removeClass('selected');
+    	this.tabs_content.find('.tab-content:eq(' +  index +')').addClass('selected');
+
+    	// Select the approparite tab 
+    	this.tabs.find('li').removeClass('selected');
+    	this.tabs.find('li:eq(' +  index +')').addClass('selected');
+	},
 	init: function(container){ 
+    	var _t            	= this;
+    	_t.container 		= container;
+
+    	_t.tabs 	 		= _t.container.children('.tabs');
+    	_t.tabs_content 	= _t.container.children('.tabs-content');
+
+
+    	// Attach events
+    	_t.tabs.find('li').each(function( index){ 
+    		$(this).on('click.creator.tabs', function() { _t._tab_clicked.apply(_t, [index]) } );
+    	});
+    	
+    	return _t;
+
+    }	
+});
+
+
+window.game_objects_collection = Object.create({
+
+	init: function(game_objects, workspace){ 
     	var _t            = this;
-    	console.log("Initialize game-object");
+
+    	_t.container 	  = workspace;
+		_t.container.find( ".go-draghandle").draggable({ revert: true, helper: "clone", appendTo: "body" });
+		_t.container.find( ".go-draghandles" ).draggable({ revert: true, helper: "clone", appendTo: "body" });
+		_t.container.find( ".go-droparea" ).droppable({ accept: ".go-draghandle", hoverClass: "go-droparea-active", drop: function(){ console.log("Dropped")}  });
+
+    	return _t;
+	}
+});
+
+window.game_object = Object.create({
+
+	init: function(game_object, workspace){ 
+    	var _t            = this;
+    	_t.container 	  = workspace;
+		_t.container.find( ".go-draghandle").draggable({ revert: true, helper: "clone", appendTo: "body" });
+		_t.container.find( ".go-draghandles" ).draggable({ revert: true, helper: "clone", appendTo: "body" });
+		_t.container.find( ".go-droparea" ).droppable({ accept: ".go-draghandle", hoverClass: "go-droparea-active", drop: function(){ console.log("Dropped")}  });
+
+		_t.tabs 	= Object.create(window.game_objects_tabs).init(_t.container.find('.gameobject-tabs'));
+		_t.editable = Object.create(window.editable).init(_t.container);
+
+
+    	return _t;
+	}
+});
+
+
+window.workspaces = Object.create({
+	_find_workspace: function(workspace){
+		return $($('.workspace')[workspace - 1]);
+	},
+	open_game_object: function(identifier, workspace){
+		// Find by ajax and send back
+
+		this._open_game_object(workspace, game_object);
+
+	},
+	_open_game_object: function(workspace, game_object){ 
+		var _t = this;
+		var ws  = _t._find_workspace(workspace);
+		
+		console.log(_t.templates);
+		// Perhaps some effect should be used to indicate interaction?
+		ws.html(_t.templates['game_object'].render(game_object));
+
+		ws.addClass('occupied');
+		Object.create(window.game_object).init(game_object, ws);
+	},
+
+	_open_game_objects_collection: function(workspace, game_objects){ 
+		var _t  = this;
+		var ws  = _t._find_workspace(workspace);
+
+			// Perhaps some effect should be used to indicate interaction?
+		ws.html(_t.templates['game_objects_collection'].render(game_objects));
+
+		ws.addClass('occupied');
+		Object.create(window.game_objects_collection).init(game_objects, ws);
+	},
+	init: function(){ 
+    	var _t            = this;
+    	console.log("Initialize workspaces");
+
+    	_t.workspaces 	= [];
+
+    	// Add templates for future references
+    	_t.templates 	= {};
+    	_t.templates['game_object'] 			= Liquid.parse($('#game_object_template').html());
+    	_t.templates['game_objects_collection'] = Liquid.parse($('#game_objects_collection_template').html());
+    	_t.templates['workspace_popin'] 		= Liquid.parse($('#workspace_popin_template').html());
+
+    	return this;
 	}
 });
--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,6 +1,9 @@
 //= require jquery-ui
+//= require jquery.miller
+//= require liquid.min
 //= require gameobject
 //= require_self
+
 
 
 
@@ -21,7 +24,7 @@ var tree = [
 	 },
 ];
 
-
+var workspace_manager;
 
 $(document).ready(function() {
 		$('#miller').miller({
@@ -43,8 +46,30 @@ $(document).ready(function() {
 			}
 		);
 
+
 		// Initialize game objects
-		$( ".go-draghandle" ).draggable({ revert: true, helper: "clone" });
+		$( ".go-draghandle" ).draggable({ revert: true, helper: "clone", appendTo: "body" });
+		$( ".go-draghandles" ).draggable({ revert: true, helper: "clone", appendTo: "body" });
 		$( ".go-droparea" ).droppable({ accept: ".go-draghandle", hoverClass: "go-droparea-active", drop: function(){ console.log("Dropped")}  });
+
+
+		// Close button on all popin
+		$("body").on('click.creator', '.popin .close',  function(){ console.log("close");$(this).parent('.popin').css({'display': 'none'})});
+		
+		// Close a workspace object 
+		$(".work-spaces").on('click.creator', '.tools .icon.x',  function(){ console.log($(this).parent('.workspace'));$(this).parents('.workspace').html(' ').removeClass('occupied')});
+
+
+		// Editable HACK
+		$(".work-spaces").on('click.creator', '.editable',  function(){ console.log("EDIT");});
+
+
+
+		workspace_manager = Object.create(window.workspaces).init();
+
+		// This is just functionallity test
+		workspace_manager._open_game_object(1, {'name' : 'Name of game object that is really long', 'identifier' : '7777777', 'properties': [{'name': 'ninjor', 'current_value' : '0', 'default_value': '2'}, {'name': 'tomtar', 'current_value' : '10', 'default_value': '0'} ], 'description' : 'Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Maecenas faucibus mollis interdum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nullam id dolor id nibh ultricies vehicula ut id elit. Etiam porta sem malesuada magna mollis euismod.', 'image_url': ''})
+		workspace_manager._open_game_object(3, {'name' : 'Min get', 'identifier' : '6666', 'properties': [{'name': 'styrkan', 'current_value' : 'etsy', 'default_value': 'nope'}, {'name': 'glädje', 'current_value' : '1', 'default_value': '0'} ], 'description' : 'loremipsum', 'image_url': ''})
+		workspace_manager._open_game_objects_collection(2, {'num_game_objects' : 13, 'game_objects_list': [{'name' : 'objekt 1', 'identifier' : '123'}, {'name' : 'objekt 2', 'identifier' : '1234'}] });
 	}
 );
